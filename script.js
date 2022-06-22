6044c022d9e7dc512d161e56680b66d50de336c7
Raazkatwal <raazkatwal5@gmail.com>
2022-06-22 20:03:55 +0545

    rickrolled

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const playpausebtn = document.querySelector(".play-pause-btn");
 const videocontainer = document.querySelector(".video-container");
 const video = document.querySelector("video");
-playpausebtn.addEventListener('click', toggleplay)
+
+playpausebtn.addEventListener('click', toggleplay);
+video.addEventListener('click', toggleplay)
+document.addEventListener("keydown", e => {
+    switch (e.key.toLowerCase()) {
+        case " ":
+        case "k":
+            toggleplay();
+            break;
+    }
+})
+
 function toggleplay() {
     video.paused ? video.play() : video.pause();
 }
